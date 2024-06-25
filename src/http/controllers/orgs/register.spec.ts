bc9cae907a7d2962787142e0913716166c055b32
Urias Pereira <uriasluis64@gmail.com>
2024-06-25 13:58:14 -0300

    register org test e2e

---
+++ src/http/controllers/orgs/register.spec.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register org', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'ORG Animals',
+      author_name: 'Animal Brown',
+      email: 'animal.brown@example.com',
+      password: '123456',
+      whatsapp: '11 953237408',
+
+      cep: '06530-075',
+      state: 'São Paulo',
+      city: 'Santana de Parnaíba',
+      neighborhood: 'Fazendinha',
+      street: 'Rua São paulo',
+
+      latitude: -23.41101,
+      longitude: -46.87399,
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
