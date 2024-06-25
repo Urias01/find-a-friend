7e6cbddae98b67b2c510d75270c5b5e8b1c14f2d
Urias Pereira <uriasluis64@gmail.com>
2024-06-25 11:05:04 -0300

    fix verify jwt

--- src/http/middlewares/verify-jwt.ts
+++ src/http/middlewares/verify-jwt.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
   try {
-    request.jwtVerify()
+    await request.jwtVerify()
   } catch (err) {
     return reply.status(401).send({ message: 'Unauthorized' })
   }
